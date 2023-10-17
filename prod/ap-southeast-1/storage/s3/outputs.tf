bc7fabd5bc7dca2ab52cdc77de623bc51267457c
shiyuan <shiyuanlee9@gmail.com>
2023-10-17 11:54:15 +0800

    add s3_bucket_website_endpoint

--- prod/ap-southeast-1/storage/s3/outputs.tf
+++ prod/ap-southeast-1/storage/s3/outputs.tf
@@ -7,3 +7,8 @@ output "s3_bucket_arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
   value       = module.website_s3_bucket.s3_bucket_arn
 }
+
+output "s3_bucket_website_endpoint" {
+  description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string."
+  value       = module.website_s3_bucket.s3_bucket_website_endpoint
+}
